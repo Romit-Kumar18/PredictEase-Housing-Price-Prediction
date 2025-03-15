b07e2265d706c01e44888fb0c1615718a86be8dd
Romit Kumar <rk8523@srmist.edu.in>
2025-03-15 09:24:45 +0530

    update: landing page

--- price-prediction-ui/app/page.js
+++ price-prediction-ui/app/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="h-max border-5 border-solid border-white-500 rounded-2px p-6 bg-[#212121] font-[family-name:var(--font-geist-mono)]">
+    <div className="h-max p-6 bg-[#212121] font-[family-name:var(--font-geist-mono)]">
       <div className="flex flex-col items-center font-[family-name:var(--font-geist-sans)] text-[#ffffff] text-4xl --drop-shadow-xl font-bold">
         <span>
           Welcome to <span className="text-[#ff9900]">PredictEase</span>
@@ -21,18 +21,17 @@ export default function Home() {
         />
       </div>
       <div className="bg-[#313131]">
-        <div className="flex flex-col place-items-center border-solid border-2 rounded-md pt-10">
-          <div className="font-bold">
-            FIND YOUR PERFECT PROPERTY IN CHENNAI
+        <div className="flex flex-col items-center justify-center border-solid border-2 rounded-md pt-10">
+          <div className="font-bold text-xl">
+            GET AN ACCURATE ESTIMATE OF HOUSE PRICES IN CHENNAI
           </div>
-          <div className="mt-2">
-            Explore a wide range of properties in Chennai tailored to your preferences.
-          </div>
-            <a href="/prediction" className="group m-5 border-2 border-solid border-white rounded-xl p-5 bg-blue-600 hover:text-orange-600 hover:bg-[#313131]">
-              <div>
-                <p>Get Started</p>
-              </div>
-            </a>
+          <div className="mt-2 max-w-xl text-center">
+            Get house price estimates in Chennai using data-driven analytics. Ideal for buyers, sellers, and investors.</div>
+          <a href="/prediction" className="group m-5 border-2 border-solid border-white rounded-xl p-5 bg-orange-600 hover:bg-blue-600 hover:scale-105">
+            <div>
+              <p>Get Started</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
